39335eae6ce1a008fabfcf0977484bfd6a680df7
Nandha1221 <111984645+Nandha1221@users.noreply.github.com>
2024-09-09 22:57:26 +0530

    Update JavaProgram.java

--- JavaProgram.java
+++ JavaProgram.java
@@ -1,7 +1,6 @@
 public class  jenkinsPush{
   public static void main(Strings[] args){
       System.out.println("Hi Nandha");
-      System.out.println("Hi Arya");
       System.out.println("Hi Surya");
       System.out.println("Hi Agni");
       System.out.println("Hi sajith");
